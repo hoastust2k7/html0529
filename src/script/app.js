14ab9b2e27cce3a2e4e2b79fb23bbc57397211e7
Asaki Yuki <vantrong2007vn@proton.me>
2025-06-13 02:54:23 +0700

    idk

--- src/script/app.js
+++ src/script/app.js
@@ -1,5 +1,18 @@
 const navBtnLine = document.querySelector(".nav-menu-icon")
+const navContent = document.querySelector(".nav-content")
 
 function toggleMenu() {
 	navBtnLine.classList.toggle("nav-menu-icon-active")
+	navContent.classList.toggle("mobile-nav-content-show")
 }
+
+function update() {
+	if (window.innerWidth < 500) {
+		navContent.classList.add("mobile-nav-content")
+	} else {
+		navContent.classList.remove("mobile-nav-content")
+	}
+}
+
+window.addEventListener("resize", update)
+update()
